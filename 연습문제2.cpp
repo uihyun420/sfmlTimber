d983436f0fa584ec5115c41069b3bbb7a3774945
박의현 <uihyun420@gmail.com>
2025-06-15 01:12:19 +0900

    연습키보드방향키구현플레이어와도끼이동

--- 연습문제2.cpp
+++ 연습문제2.cpp
@@ -15,6 +15,13 @@ int main()
 	(int) : time() 함수가 반환하는 값을 int형으로 형변환
 	*/
 
+
+
+
+
+
+
+
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Timber!");
 
 	sf::Texture textureBackground;
@@ -59,7 +66,7 @@ int main()
 	spriteBee.setTexture(textureBee);
 	spriteBee.setOrigin(0.f, 0.f);
 	spriteBee.setPosition(50, 850);
-	
+
 	sf::Vector2f Beedirection;
 	float spriteSpeedBee;
 
@@ -128,9 +135,18 @@ int main()
 	sf::Sprite spriteAxe;
 	spriteAxe.setTexture(textureAxe);
 	spriteAxe.setOrigin(textureAxe.getSize().x * 0.5f, textureAxe.getSize().y * 0.5f);
-	spriteAxe.setPosition(spritePlayer.getPosition().x + texturePlayer.getSize().x * 0.5f - 50.f, spritePlayer.getPosition().y);    
+	spriteAxe.setPosition(spritePlayer.getPosition().x + texturePlayer.getSize().x * 0.5f - 50.f, spritePlayer.getPosition().y);
 	// 도끼가 플레이어를 따라다니도록, setposition = 캐릭터 오른쪽 끝
 
+
+
+
+
+
+
+		// 키보드 추가할 때 선언
+	bool isLeft = false;       // 왼쪽 키가 눌린 상태인지
+	bool isRight = false;      // 오른쪽 키가 눌린 상태인지
 
 
 
@@ -139,112 +155,184 @@ int main()
 
 
 
-		while (window.isOpen())
+	while (window.isOpen())
+	{
+		sf::Time time = clock.restart();
+		float deltaTime = time.asSeconds();
+
+
+		bool isLeftDown = false;   // 왼쪽 키가 눌린 순간 한 번만 반응하기 위한 플래그            왼쪽 방향키가 딱 한 번 눌린 순간에만 true
+		bool isRightDown = false;  // 오른쪽 키가 눌린 순간 한 번만 반응하기 위한 플래그		 오른쪽 방향키가 딱 한 번 눌린 순간에만 true
+
+		bool isLeftUp = false;     // 왼쪽 키가 떼어진 순간 한 번만 반응하기 위한 플래그          왼쪽 방향키를 딱 한 번 뗀 순간에만 true
+		bool isRightUp = false;    // 오른쪽 키가 떼어진 순간 한 번만 반응하기 위한 플래그       오른쪽 방향키를 딱 한 번 뗀 순간에만 true 
+
+
+		sf::Event event;
+		while (window.pollEvent(event))
 		{
-			sf::Time time = clock.restart();
-			float deltaTime = time.asSeconds();
-
-			sf::Event event;
-			while (window.pollEvent(event))
+			switch (event.type)
 			{
-				if (event.type == sf::Event::Closed)
-					window.close();
-			}
+			case sf::Event::Closed:
+				window.close();
+				break;
 
-
-
-
-
-			//---------------------------------------- 업데이트 ----------------------------------------------------------------------------
-			for (int i = 0; i < 3; i++)
-			{
-				sf::Vector2f position = spriteBackgroundElement[i].getPosition();
-				// Vector2f 위치, 방향, 속도 등 표현하는 타입
-				// 변수이름(position) 선언,  getPosition(); 구름의 현재 위치를 가져옴
-
-				position += directionElement[i] * speedElement[i] * deltaTime;
-				// 구름의 위치를 시간에 따라 자연스럽게 이동시킴 / 한 프레임에서 다음 프레임까지의 시간
-				// 방향 x 속도 x 시간 = 이동 거리
-				// 즉 position은 현재 구름의 포지션에 이동 거리를 할당하는 것.
-
-				spriteBackgroundElement[i].setPosition(position);
-				// i번째 구름을 position 좌표로 이동시켜라. 라는 의미 
-
-				if (position.x < -200 || position.x>1920 + 200)
-					// 화면 해상도가 1920*1080이기 때문에 -200은 화면 밖, 2120은 화면 오른쪽 밖
-					// 화면 밖으로 나갔는지의 가정
+			case sf::Event::KeyPressed:
+				switch (event.key.code)
 				{
-					float random = (float)rand() / RAND_MAX;  // 0.0 ~ 1.0 사이의 난수 설정 
-					if (random < 0.5f)
+				case sf::Keyboard::Left:         // 왼쪽 방향키가 눌렸을 때 
+					if (!isLeft)                 // 만약 이전 왼쪽 방향키가 눌리지 않았다면
 					{
-						directionElement[i].x = 1.f; // 오른쪽 이동
-						spriteBackgroundElement[i].setScale(-1.f, 1.f); // 좌우 반전
-						spriteBackgroundElement[i].setPosition(-150, rand() % 580 + 10);
+						isLeftDown = true;       // 왼쪽 키가 처음 눌린 순간임을 표시 
 					}
-					else
+					isLeft = true;               // 왼쪽 키가 눌려있는 상태임을 표시
+					break;
+
+
+				case sf::Keyboard::Right:
+					if (!isRight)
 					{
-						directionElement[i].x = -1.f; // 왼쪽으로 이동
-						spriteBackgroundElement[i].setScale(1.f, 1.f);
-						spriteBackgroundElement[i].setPosition(1920 + 200, rand() % 580 + 10);
+						isRightDown = true;
 					}
+					isRight = true;
+					break;
 				}
+				break;
 
-				speedElement[i] = rand() % 15 + 0.5f;
-
-
-			}
-			sf::Vector2f beePosition = spriteBee.getPosition(); // 벌의 현재 위치
-			beePosition += Beedirection * spriteSpeedBee * deltaTime;
-			spriteBee.setPosition(beePosition);
-
-
-			if (beePosition.x < -200 || beePosition.x> 1920 + 200)
-			{
-				spriteSpeedBee = rand() % 5 + 5;
-
-				float randomDirectionBee = (float)rand() / RAND_MAX;
-
-				if (randomDirectionBee < 0.5f)
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
 				{
-					Beedirection.x = -1.f;
-					spriteBee.setScale(1.f, 1.f);
-					spriteBee.setPosition(1920 + 150, 850);
+				case sf::Keyboard::Left:       // 왼쪽 방향키가 떼어졌을 때
+					isLeft = false;            // 왼쪽 키가 눌리지 않은 상태임을 표시
+					isLeftUp = true;           // 왼쪽 키가 떼어진 순간 임을 표시 
+					break;
+
+				case sf::Keyboard::Right:      
+					isRight = false; 
+					isRightUp = true;
+					break;
+				}
+				break;
+			}
+
+
+
+
+
+		}
+
+
+
+
+
+		//---------------------------------------- 업데이트 ----------------------------------------------------------------------------
+		for (int i = 0; i < 3; i++)
+		{
+			sf::Vector2f position = spriteBackgroundElement[i].getPosition();
+			// Vector2f 위치, 방향, 속도 등 표현하는 타입
+			// 변수이름(position) 선언,  getPosition(); 구름의 현재 위치를 가져옴
+
+			position += directionElement[i] * speedElement[i] * deltaTime;
+			// 구름의 위치를 시간에 따라 자연스럽게 이동시킴 / 한 프레임에서 다음 프레임까지의 시간
+			// 방향 x 속도 x 시간 = 이동 거리
+			// 즉 position은 현재 구름의 포지션에 이동 거리를 할당하는 것.
+
+			spriteBackgroundElement[i].setPosition(position);
+			// i번째 구름을 position 좌표로 이동시켜라. 라는 의미 
+
+			if (position.x < -200 || position.x>1920 + 200)
+				// 화면 해상도가 1920*1080이기 때문에 -200은 화면 밖, 2120은 화면 오른쪽 밖
+				// 화면 밖으로 나갔는지의 가정
+			{
+				float random = (float)rand() / RAND_MAX;  // 0.0 ~ 1.0 사이의 난수 설정 
+				if (random < 0.5f)
+				{
+					directionElement[i].x = 1.f; // 오른쪽 이동
+					spriteBackgroundElement[i].setScale(-1.f, 1.f); // 좌우 반전
+					spriteBackgroundElement[i].setPosition(-150, rand() % 580 + 10);
 				}
 				else
 				{
-					Beedirection.x = 1.f;
-					spriteBee.setScale(-1.f, 1.f);
-					spriteBee.setPosition(-150, 850);
+					directionElement[i].x = -1.f; // 왼쪽으로 이동
+					spriteBackgroundElement[i].setScale(1.f, 1.f);
+					spriteBackgroundElement[i].setPosition(1920 + 200, rand() % 580 + 10);
 				}
 			}
-			
 
-			if (isPlayerAxeLeft)
+			speedElement[i] = rand() % 15 + 0.5f;
+
+
+		}
+		sf::Vector2f beePosition = spriteBee.getPosition(); // 벌의 현재 위치
+		beePosition += Beedirection * spriteSpeedBee * deltaTime;
+		spriteBee.setPosition(beePosition);
+
+
+		if (beePosition.x < -200 || beePosition.x> 1920 + 200)
+		{
+			spriteSpeedBee = rand() % 5 + 5;
+
+			float randomDirectionBee = (float)rand() / RAND_MAX;
+
+			if (randomDirectionBee < 0.5f)
 			{
-				spriteAxe.setPosition(spritePlayer.getPosition().x - texturePlayer.getSize().x * 0.5f + 50.f, spritePlayer.getPosition().y);
-				spriteAxe.setScale(-1.f, 1.f);
+				Beedirection.x = -1.f;
+				spriteBee.setScale(1.f, 1.f);
+				spriteBee.setPosition(1920 + 150, 850);
 			}
 			else
 			{
-				spriteAxe.setPosition(spritePlayer.getPosition().x + texturePlayer.getSize().x * 0.5f + 50.f, spritePlayer.getPosition().y);
-				spriteAxe.setScale(1.f, 1.f);
+				Beedirection.x = 1.f;
+				spriteBee.setScale(-1.f, 1.f);
+				spriteBee.setPosition(-150, 850);
 			}
-
-
-			window.clear();
-			window.draw(spriteBackground);
-
-			for (int i = 0; i < 3; i++)
-			{
-				window.draw(spriteBackgroundElement[i]);
-			}
-
-			window.draw(spriteTree);
-			window.draw(spriteBee);
-			window.draw(spritePlayer);
-			window.draw(spriteAxe);
-
-			window.display();
 		}
+
+
+
+		if (isLeftDown)
+		{
+			spritePlayer.setPosition(1920 * 0.5f - 200, 950);        // 플레이어가 중앙에서 왼쪽으로 이동 
+			isPlayerAxeLeft = true;                                  // 이때 도끼는 왼쪽에 있어야 하므로 상태 저장
+		}
+		else if(isRightDown)
+		{
+			spritePlayer.setPosition(1920 * 0.5f + 200, 950);        // 플레이어가 중앙에서 오른쪽으로 이동 
+			isPlayerAxeLeft = false;                                  // 이때 도끼는 오른쪽에 있어야 하므로 상태 저장
+		}
+
+
+
+		if (isPlayerAxeLeft)
+		{
+			spriteAxe.setPosition(spritePlayer.getPosition().x - texturePlayer.getSize().x * 0.5f + 50.f, spritePlayer.getPosition().y);
+			spriteAxe.setScale(-1.f, 1.f);
+		}
+		else
+		{
+			spriteAxe.setPosition(spritePlayer.getPosition().x + texturePlayer.getSize().x * 0.5f + 50.f, spritePlayer.getPosition().y);
+			spriteAxe.setScale(1.f, 1.f);
+		}
+
+
+
+
+
+		// 그리기
+		window.clear();
+		window.draw(spriteBackground);
+
+		for (int i = 0; i < 3; i++)
+		{
+			window.draw(spriteBackgroundElement[i]);
+		}
+
+		window.draw(spriteTree);
+		window.draw(spriteBee);
+		window.draw(spritePlayer);
+		window.draw(spriteAxe);
+
+		window.display();
 	}
+}
 
